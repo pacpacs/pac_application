713469c71ffa2586758412a63868cdded2c32ab8
yanggaeng <carys3115@ajou.ac.kr>
2019-07-01 17:19:31 +0900

    appBar
    불필요한 메뉴부분 삭제

--- pac_app/lib/fixed/appBar.dart
+++ pac_app/lib/fixed/appBar.dart
@@ -6,37 +6,6 @@ class appBar {
     // TODO: status 에 따라서 login 전/후 만들기
     return new AppBar(
       backgroundColor: Colors.white,
-      leading: PopupMenuButton(
-        icon: Icon(Icons.menu, color: Colors.black),
-        itemBuilder: (BuildContext context) {
-          return [
-            PopupMenuItem( //TODO : 텍스트 추가하기
-              child: IconButton(
-                icon: Icon(Icons.fastfood),
-                onPressed: () {},
-              ),
-            ),
-            PopupMenuItem(
-              child: IconButton(
-                icon: Icon(Icons.people),
-                onPressed: () {},
-              ),
-            ),
-            PopupMenuItem(
-              child: IconButton(
-                icon: Icon(Icons.account_circle),
-                onPressed: () {},
-              ),
-            ),
-            PopupMenuItem(
-              child: IconButton(
-                icon: Icon(Icons.settings),
-                onPressed: () {},
-              ),
-            ),
-          ];
-        },
-      ),
       actions: <Widget>[
         //login button
         FlatButton(
