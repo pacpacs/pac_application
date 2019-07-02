a98c4a69ee934ce3ceeeb4f2b9752c2c6870372e
yanggaeng <carys3115@ajou.ac.kr>
2019-07-02 16:07:23 +0900

    homePage 사진 parallaxImage 에서 Carousal로 변경

--- pac_app/lib/pages/homePage.dart
+++ pac_app/lib/pages/homePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:parallax_image/parallax_image.dart';
+
+//import 'package:parallax_image/parallax_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 /**
  * P&C의 메인을 구성하는 homePage
@@ -8,6 +10,66 @@ import 'package:parallax_image/parallax_image.dart';
  * @version 1.0, P&C 의 homePage
  * @date 2019.07.02
  */
+
+final List<String> imgList = [ //TODO: 서버사진리스트로 만들기 
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+final List child = map<Widget>(
+  imgList,
+  (index, i) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(children: <Widget>[
+          //Image.network(i, fit: BoxFit.cover, width: 1000.0),
+          Image.network(i, fit: BoxFit.cover, width: 1000.0),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(200, 0, 0, 0),
+                    Color.fromARGB(0, 0, 0, 0)
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                'No. $index image',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  },
+).toList();
+
+List<T> map<T>(List list, Function handler) {
+  List<T> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add(handler(i, list[i]));
+  }
+
+  return result;
+}
 
 class homePage extends StatelessWidget {
   TextEditingController editingController = TextEditingController();
@@ -29,7 +91,7 @@ class homePage extends StatelessWidget {
           child: Text('Pick & Cook', //TODO: Text 크기 유동성있게 만들기
               style: TextStyle(
                   fontWeight: FontWeight.w800,
-                  fontSize: 40,
+                  fontSize: 30,
                   fontStyle: FontStyle.italic)),
         ),
         Padding(
@@ -51,38 +113,17 @@ class homePage extends StatelessWidget {
               'Today\'s Recommended Recipe',
               style: theme.textTheme.title,
             )),
-        new Container(
-          padding: const EdgeInsets.symmetric(vertical: 10.0),
-          constraints: const BoxConstraints(maxHeight: 200.0),
-          child: new ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: _buildHorizontalChild,
-          ),
-        ),
+        CarouselSlider(
+          items: child,
+          enableInfiniteScroll: true,
+          autoPlay: true  ,
+          enlargeCenterPage: true,
+          viewportFraction: 0.9,
+          aspectRatio: 2.0,
+
+        )
       ],
     );
   }
 
-  /**
-   * 사진을 옆으로 넘겨 볼 수 있게 해줌
-   *
-   * @ param
-   *          BuildContext context : 현재 context
-   *          int index : 현재 사진 index
-   * @ return ParallaxImage
-   */
-
-  Widget _buildHorizontalChild(BuildContext context, int index) {
-    index++;
-    if (index > 7) return null;
-    return new Padding(
-      padding: const EdgeInsets.only(right: 10.0),
-      child: new ParallaxImage(
-        extent: 100.0,
-        image: new ExactAssetImage(
-          'images/img$index.jpg', //TODO: 유동성 있는 사진 불러오게 하기
-        ),
-      ),
-    );
-  }
 }
