3f19f1d4a1f75fd44be4bc0411f79604aa6fe588
seoreal <dltjdp9792@naver.com>
2019-07-04 13:42:37 +0900

    화면 이동 버튼

--- pac_app/lib/main.dart
+++ pac_app/lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:pac_app/pages/homePage.dart';
 
 import 'fixed/appBar.dart';
 import 'fixed/bottomNavigator.dart';
-
+import 'pages/ingredients/ingredients.dart' as ingredient;
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        initialRoute: '/',
-        routes: {
-          //TODO: route사용이 안된다 되는 방법을 확인해보자
-          '/': (context) => MyHomePage(),
-//          '/login': (context) => Login(),
-//          '/register': (context) => Register(),
-        });
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
@@ -33,13 +27,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBar.getAppBar(context, ''),
         body: Center(
-          child: homePage()
+
+          child: RaisedButton(
+            child: Text("재료 선택"),
+              onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+                      return new ingredient.Choice();
+                    }
+                  )
+                );
+              },
+          ),
         ),
-        bottomNavigationBar: BottomNavigator());
+        bottomNavigationBar: BottomNavigator()
+    );
   }
 }
