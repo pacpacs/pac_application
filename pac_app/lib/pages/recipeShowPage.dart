8cabe2a18c02be7d98b36f92b1d476193fb7b438
yanggaeng <carys3115@ajou.ac.kr>
2019-07-05 00:45:35 +0900

    recipe show page container로 layout 잡기

---
+++ pac_app/lib/pages/recipeShowPage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class recipeShowPage extends StatefulWidget {
+  @override
+  _recipeShowPageState createState() => _recipeShowPageState();
+}
+
+class _recipeShowPageState extends State<recipeShowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          child: Row(
+            children: <Widget>[
+              Container(padding: const EdgeInsets.all(0.0),
+                color: Colors.orange,
+                width: 80.0,
+                height: 80.0,),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(padding: const EdgeInsets.all(0.0),
+                      color: Colors.blue,
+                      width: 80.0,
+                      height: 80.0,),
+                    Container(padding: const EdgeInsets.all(0.0),
+                      color: Colors.cyanAccent,
+                      width: 80.0,
+                      height: 80.0,)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(padding: const EdgeInsets.all(0.0),
+          color: Colors.red,
+          width: 80.0,
+          height: 80.0,)
+      ],
+    );
+  }
+}
