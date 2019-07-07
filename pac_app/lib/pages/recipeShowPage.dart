871eae8c1c8e210d77a179be7531923519a2f1e5
yanggaeng <carys3115@ajou.ac.kr>
2019-07-08 01:41:20 +0900

    기본 레시피명, 작성자사진, info 를 출력할 수 있게 레이아웃 구성, 카드부분구성

--- pac_app/lib/pages/recipeShowPage.dart
+++ pac_app/lib/pages/recipeShowPage.dart
@@ -11,31 +11,84 @@ class _recipeShowPageState extends State<recipeShowPage> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(
-          child: Row(
-            children: <Widget>[
-              Container(padding: const EdgeInsets.all(0.0),
-                color: Colors.orange,
-                width: 80.0,
-                height: 80.0,),
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    Container(padding: const EdgeInsets.all(0.0),
-                      color: Colors.blue,
-                      width: 80.0,
-                      height: 80.0,),
-                    Container(padding: const EdgeInsets.all(0.0),
-                      color: Colors.cyanAccent,
-                      width: 80.0,
-                      height: 80.0,)
-                  ],
-                ),
-              )
-            ],
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            child: Row(
+              children: <Widget>[
+                Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.brown.shade800,
+                      radius: 50,
+                      child: Text('AH'),
+                    )),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(" Recipe Title ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 30,
+                                fontStyle: FontStyle.italic)),
+                      ),
+                      Container(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: new BoxDecoration(
+                                      border:
+                                          new Border.all(color: Colors.black)),
+                                  width: 100,
+                                  child: Center(
+                                    child: Text(" Info1",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15,
+                                            fontStyle: FontStyle.italic)),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: new BoxDecoration(
+                                      border:
+                                          new Border.all(color: Colors.black)),
+                                  width: 100,
+                                  child: Center(
+                                    child: Text(" Info2",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15,
+                                            fontStyle: FontStyle.italic)),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-        Container(child: cardForm(new Card()))
+        Divider(color: Colors.black87),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Container(
+              decoration: new BoxDecoration(
+                  border: new Border.all(color: Colors.black)),
+              width: 300,
+              height: 200,
+              child: cardForm(new Card())),
+        )
       ],
     );
   }
