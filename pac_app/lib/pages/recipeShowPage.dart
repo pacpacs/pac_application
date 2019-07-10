2d23a80033df49cc902995e86ce369d634248b24
yanggaeng <carys3115@ajou.ac.kr>
2019-07-10 11:33:26 +0900

    flexible layout으로 변경

--- pac_app/lib/pages/recipeShowPage.dart
+++ pac_app/lib/pages/recipeShowPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pac_app/fixed/card/cardForm.dart';
 import 'package:pac_app/fixed/profile/User.dart';
 import 'package:pac_app/model/UserModel.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class recipeShowPage extends StatefulWidget {
   User user = new User(
@@ -25,73 +26,77 @@ class _recipeShowPageState extends State<recipeShowPage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                  padding: const EdgeInsets.all(10.0),
-                  child: widget.user.getProfileImage(50)),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(" Receipe Title ",
-                      overflow: TextOverflow.visible,  
-                          style: TextStyle(
-                              fontWeight: FontWeight.w800,
-                              fontSize: 30,
-                              fontStyle: FontStyle.italic)),
-                    ),
-                    Container(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Row(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: new BoxDecoration(
-                                    border:
-                                        new Border.all(color: Colors.black)),
-                                child: Center(
-                                  child: Text(" Info1",
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: widget.user.getProfileImage(50)),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        child: AutoSizeText(" Recipe Title ",
+                            overflow: TextOverflow.fade,
+                            maxLines: 1,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 28,
+                                fontStyle: FontStyle.italic)),
+                      ),
+                      Container(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  child: AutoSizeText(" Info1",
+                                      maxLines: 1,
                                       style: TextStyle(
                                           fontWeight: FontWeight.w400,
                                           fontSize: 15,
                                           fontStyle: FontStyle.italic)),
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: new BoxDecoration(
-                                    border:
-                                        new Border.all(color: Colors.black)),
-                                child: Center(
-                                  child: Text(" Info2",
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  child: AutoSizeText(" Info2",
+                                      maxLines: 1,
                                       style: TextStyle(
                                           fontWeight: FontWeight.w400,
                                           fontSize: 15,
                                           fontStyle: FontStyle.italic)),
                                 ),
                               ),
-                            ),
-                          ],
-                        ))
-                  ],
+                            ],
+                          ))
+                    ],
+                  ),
                 ),
               )
             ],
           ),
         ),
         Divider(color: Colors.black87),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25.0),
-          child: Container(
-              decoration: new BoxDecoration(
-                  border: new Border.all(color: Colors.black)),
-              width: 300,
-              height: 200,
-              child: cardForm(new Card())),
+        Flexible(
+          flex: 2,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Container(
+                decoration: new BoxDecoration(
+                    border: new Border.all(color: Colors.black)),
+                width: 300,
+                height: 200,
+                child: cardForm(new Card())),
+          ),
         )
       ],
     );
