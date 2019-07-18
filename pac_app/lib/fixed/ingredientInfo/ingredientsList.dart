34e6bbe2e730a831efea0e1b52b7db7eef6a70db
seoreal <dltjdp9792@naver.com>
2019-07-18 20:38:46 +0900

    음식 선택 버튼 제작

--- pac_app/lib/fixed/ingredientInfo/ingredientsList.dart
+++ pac_app/lib/fixed/ingredientInfo/ingredientsList.dart
@@ -28,9 +28,34 @@ class _IngredientsListState extends State<IngredientsList> {
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  //TODO : 공통되게 사용하는 색깔 fixed 따로 빼야하지 않을까?
+                    new FloatingActionButton(
+                        backgroundColor: Colors.red.shade800,
+                        onPressed: (){},
+                        child: new Text("육류")),
+                    new FloatingActionButton(
+                        backgroundColor: Colors.green.shade900,
+                        onPressed: (){},
+                        child: new Text("채소")),
+                    new FloatingActionButton(
+                        backgroundColor: Colors.brown,
+                        onPressed: (){},
+                        child: new Text("곡물")),
+                    new FloatingActionButton(
+                        backgroundColor: Colors.deepOrangeAccent[100],
+                        onPressed: (){},
+                        child: new Text("양념")),                        
+                    new FloatingActionButton(
+                        backgroundColor: Colors.black,
+                        onPressed: (){},
+                        child: new Text("기타")),
+              ],),
               new Expanded(child: new ListView(
                 padding: new EdgeInsets.symmetric(vertical: 8.0),
-                children: widget.ingredient.where((ingredient)=>ingredient.categoryCode == [1,2]).map((Ingredient ingredient) {
+                children: widget.ingredient.where((ingredient)=>ingredient.categoryCode != 1).map((Ingredient ingredient) {
                   return IngredientItem(ingredient);
                 }).toList(),
               )),
