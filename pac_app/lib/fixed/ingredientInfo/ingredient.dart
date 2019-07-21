a2121a5ebb3ac15f372262bc22eee6d7b64f7f09
seoreal <dltjdp9792@naver.com>
2019-07-21 19:10:08 +0900

    0~4까지 code 숫자 변경, ingredient 정보를 setcolor,setText로 받아올 수 있게 만듬.

--- pac_app/lib/fixed/ingredientInfo/ingredient.dart
+++ pac_app/lib/fixed/ingredientInfo/ingredient.dart
@@ -1,6 +1,48 @@
-class Ingredient{
+import 'package:flutter/material.dart';
+
+enum categoryCodeNumber { meat, vegitable, grain, seasoning, etc }
+
+class Ingredient {
   String name;
   int categoryCode;
   bool isCheck;
   Ingredient(this.name, this.categoryCode, this.isCheck);
+  setColor() {
+    int code = this.categoryCode;
+    switch (code) {
+      case 0: //육류
+        return Colors.red.shade800;
+        break;
+      case 1: //채소
+        return Colors.green.shade900;
+        break;
+      case 2: //탄수화물
+        return Colors.brown;
+        break;
+      case 3: //향신료
+        return Colors.deepOrangeAccent[100];
+        break;
+      default:
+        return Colors.black;
+    }
+  }
+  setCategory(){
+    int code = this.categoryCode;
+    switch(code){
+      case 0: //육류
+        return "육류";
+        break;
+      case 1: //채소
+        return "채소";
+        break;
+      case 2: //탄수화물
+        return "곡물";
+        break;
+      case 3: //향신료
+        return "양념";
+        break;
+      default:
+        return "기타";
+    }
+  }
 }
