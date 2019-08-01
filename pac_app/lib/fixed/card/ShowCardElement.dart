449288b6802a9151f1195af06242433f17b09fe5
suhyunNam000 <nsh403@gmail.com>
2019-08-01 14:24:23 +0900

    showCardElement와 CardForm 합병

--- pac_app/lib/fixed/card/ShowCardElement.dart
+++ pac_app/lib/fixed/card/ShowCardElement.dart
@@ -1,31 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:pac_app/fixed/card/CardElement.dart';
+import 'package:pac_app/model/RecipeModel.dart';
+import 'package:provider/provider.dart';
 
-class ShowCardElement extends CardElement {
+class ShowCardElement extends StatelessWidget with ChangeNotifier {
   String imageUrl;
   String recipeDescription;
-  ShowCardElement({Key key, @required this.imageUrl, @required this.recipeDescription});
+  List<RecipeModel> recipe;
+  ShowCardElement(
+      {Key key,
+      @required this.recipe});
+
+//TODO : ChangeNoti implement
+
+int _idx=0;
+  get idx => _idx;
+  set idx(int newIdx) {
+    _idx = newIdx;
+    imageUrl = recipe[_idx].imgPath;
+    recipeDescription = recipe[_idx].description;
+    notifyListeners();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final changer = Provider.of<ShowCardElement>(context);
     return Center(
-      child: ListView(children: <Widget>[
-        SizedBox(
-          width: 120,
-          height: 180,
-          child: Image.network(
-            imageUrl,
-            fit: BoxFit.contain,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Text(
-            recipeDescription,
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ]),
-    );
+        child: Container(
+            child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: new Stack(
+                  children: <Widget>[
+                    new Container(
+                      child: ListView(children: <Widget>[
+                        SizedBox(
+                          width: 120,
+                          height: 180,
+                          child: Image.network(
+                            recipe[0].imgPath,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            recipe[0].description,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ]),
+                    ),
+                    new Container(
+                        child: Align(
+                      alignment: Alignment(-1.1, 0),
+                      child: IconButton(
+                        iconSize: 40,
+                        icon: Icon(Icons.arrow_left),
+                        onPressed: () {
+                          
+                          if (_idx > 0) {
+                              _idx--;
+                              _idx=changer.idx;
+                              print('--index : $_idx');
+                            }
+                        },
+                      ),
+                    )),
+                    new Container(
+                        child: Align(
+                      alignment: Alignment(1.1, 0),
+                      child: IconButton(
+                        iconSize: 40,
+                        icon: Icon(Icons.arrow_right),
+                        onPressed: () {
+                          if (_idx < recipe.length)
+                              
+                              _idx=changer.idx;
+                              _idx++;
+                            print('++index : $_idx');
+                        },
+                      ),
+                    )),
+                  ],
+                ))));
   }
 }
--- pac_app/lib/pages/recipeShowPage.dart
+++ pac_app/lib/pages/recipeShowPage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pac_app/fixed/card/CardForm.dart';
 import 'package:pac_app/fixed/card/ShowCardElement.dart';
 import 'package:pac_app/fixed/profile/User.dart';
 import 'package:pac_app/model/UserModel.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:pac_app/model/RecipeModel.dart';
+import 'package:provider/provider.dart';
 
 class RecipeShowPage extends StatefulWidget {
   User user = new User(
@@ -14,93 +14,126 @@ class RecipeShowPage extends StatefulWidget {
           nickName: "syk",
           profileImgPath:
               "https://pbs.twimg.com/profile_images/965791773522984960/QhuhU3pp_400x400.jpg"));
-  
-  static List<RecipeModel> dummy1 = [new RecipeModel(
-    author: '남수',
-    createdDate: DateTime.now(),
-    description: '재료를 잘게썬다.\n한 입 크기로 자르는 것이 중요.',
-    id: '123asdf',
-    imgPath: 'https://i.imgur.com/innssyj.png',
-    kcal: 400,
-    orderNum: 1,
-    title: '짱 맛있는 샥슈카-에그인헬')];
+
+  static List<RecipeModel> dummy1 = [
+    new RecipeModel(
+        author: '남수',
+        createdDate: DateTime.now(),
+        description: '재료를 잘게썬다.\n한 입 크기로 자르는 것이 중요.',
+        id: '123asdf',
+        imgPath: 'https://i.imgur.com/innssyj.png',
+        kcal: 400,
+        orderNum: 1,
+        title: '짱 맛있는 샥슈카-에그인헬'),
+    new RecipeModel(
+        author: '남수',
+        createdDate: DateTime.now(),
+        description: '월급을 잘게썬다.\n한 통장 크기로 자르는 것이 중요.',
+        id: '123asdf',
+        imgPath: 'https://i.imgur.com/RmtzZ85.jpg',
+        kcal: 400,
+        orderNum: 1,
+        title: '짱 맛있는 샥슈카-에그인헬')
+  ];
 
   RecipeModelList dummylist = new RecipeModelList(recipeModelList: dummy1);
-  
+
   @override
-  _RecipeShowPageState createState() => _RecipeShowPageState(dummyrecipeList: dummylist);
+  _RecipeShowPageState createState() =>
+      _RecipeShowPageState(dummyrecipeList: dummylist);
 }
 
-class _RecipeShowPageState extends State<RecipeShowPage> {
+class _RecipeShowPageState extends State<RecipeShowPage> with ChangeNotifier {
   final RecipeModelList dummyrecipeList;
+  int _idx = 0;
+  String imgPath, desc;
   _RecipeShowPageState({Key key, @required this.dummyrecipeList});
+
+  get idx => _idx;
+  set idx(int newIdx) {
+    _idx = newIdx;
+    imgPath = dummyrecipeList.recipeModelList[_idx].imgPath;
+    desc = dummyrecipeList.recipeModelList[_idx].description;
+    notifyListeners();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(child: widget.user.getProfileImage(50)),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        child: AutoSizeText(dummyrecipeList.recipeModelList[0].title,
-                            overflow: TextOverflow.fade,
-                            maxLines: 1,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800,
-                                fontSize: 28,
-                                fontStyle: FontStyle.italic)),
-                      ),
-                      Container(
-                          child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              child: AutoSizeText(dummyrecipeList.recipeModelList[0].author,
+    return ChangeNotifierProvider(
+      builder: (context) => ShowCardElement(
+        recipe: dummyrecipeList.recipeModelList,
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(child: widget.user.getProfileImage(50)), //글쓴이 정보
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: AutoSizeText(
+                              dummyrecipeList.recipeModelList[0].title,
+                              overflow: TextOverflow.fade,
+                              maxLines: 1,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 28,
+                                  fontStyle: FontStyle.italic)),
+                        ),
+                        Container(
+                            child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                child: AutoSizeText(
+                                    dummyrecipeList.recipeModelList[0].author,
+                                    maxLines: 1,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 15,
+                                        fontStyle: FontStyle.italic)),
+                              ),
+                            ),
+                            Container(
+                              child: AutoSizeText(
+                                  dummyrecipeList.recipeModelList[0].createdDate
+                                      .toString(),
                                   maxLines: 1,
                                   style: TextStyle(
                                       fontWeight: FontWeight.w400,
                                       fontSize: 15,
                                       fontStyle: FontStyle.italic)),
                             ),
-                          ),
-                          Container(
-                            child: AutoSizeText(dummyrecipeList.recipeModelList[0].createdDate.toString(),
-                                maxLines: 1,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 15,
-                                    fontStyle: FontStyle.italic)),
-                          ),
-                        ],
-                      ))
-                    ],
+                          ],
+                        ))
+                      ],
+                    ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
-        ),
-        Flexible(
-          flex: 2,
-          child: Padding(
-              padding:
-                const EdgeInsets.symmetric(vertical: 25.0, horizontal: 20.0),
-              child: Container(child: CardForm(ShowCardElement(imageUrl: dummyrecipeList.recipeModelList[0].imgPath, recipeDescription: dummyrecipeList.recipeModelList[0].description,)))),
-        )
-      ],
+          Flexible(
+            flex: 2,
+            child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 25.0, horizontal: 20.0),
+                child: ShowCardElement(
+                  recipe: dummyrecipeList.recipeModelList,
+                )),
+          )
+        ],
+      ),
     );
   }
 }
