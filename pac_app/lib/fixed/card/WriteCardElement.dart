ff2114c388a0b71faf9a58e6ecf821f082082ab4
suhyunNam000 <nsh403@gmail.com>
2019-09-05 10:47:42 +0900

    RecipeStep 이미지 추가 시, Flat Button에 세팅.

--- pac_app/lib/fixed/card/WriteCardElement.dart
+++ pac_app/lib/fixed/card/WriteCardElement.dart
@@ -1,32 +1,60 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'package:pac_app/bloc/setWriteCardBloc.dart';
 import 'package:pac_app/model/RecipeModel.dart';
 import 'package:pac_app/model/RecipeProcessModel.dart';
 import 'package:pac_app/fixed/card/ShowCardBloc.dart';
-import 'package:pac_app/bloc/setWriteCardState.dart';
 
-class WriteCardElement extends StatelessWidget {
-  List<RecipeModel> recipe = new List<RecipeModel>();
-  var _image=Image.asset('images/recipeStepImagePicker.jpg');
-  List<RecipeProcessModel> recipeStep;
+class WriteCardElement extends StatefulWidget {
+  @override
+  _WriteCardElementState createState() => _WriteCardElementState();
+}
+
+class _WriteCardElementState extends State<WriteCardElement> {
+
+  RecipeModel recipe = new RecipeModel();
+  List<WriteRecipeProcessModel> recipeStep =
+      new List<WriteRecipeProcessModel>();
+  List<File> recipeStepImage = new List<File>();
+  var _image = Image.asset('images/recipeStepImagePicker.jpg');
 
   TextEditingController _txtController = new TextEditingController(text: '');
 
-  Future getimage() async {
+  Future getImageFromGallery(int index) async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-//stateful에서는 setState(_이미지=이미지) 였다.
-if(image != null) _image = Image.file(image);
-    print('이미지:'+_image.toString());
+    if (image != null) {
+      setImageList(index, image.toString());
+      setState(() {
+        _image = Image.file(image);
+        print(image.toString());
+      });
+    }
+  }
+
+  setImageList(int index, String imagePath) {
+    //recipeStepImage Setting
+    if (index >= recipeStepImage.length) {
+      recipeStepImage.add(File(imagePath));
+    } else {
+      recipeStepImage[index] = File(imagePath);
+    }
+    //recipeStep Setting
+    if (index >= recipeStep.length) {
+      recipeStep.add(new WriteRecipeProcessModel(
+          null, imagePath, null, null, null, null, null));
+    } else {
+      recipeStep[index].stre_step_image_url = imagePath;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final ShowCardBloc showCardBloc = BlocProvider.of<ShowCardBloc>(context);
-    
+
     return BlocBuilder<ShowCardBloc, int>(
       builder: (context, _idx) {
         int _idxMax = 1;
@@ -48,9 +76,7 @@ if(image != null) _image = Image.file(image);
                             child: FlatButton(
                               child: _image,
                               onPressed: () => {
-                                //TODO:버튼 이미지 변경하는 Bloc call
-                                // setWriteCardBloc.dispatch(WriteCardEvent.imageApply)
-                                getimage(),
+                                getImageFromGallery(_idx),
                               },
                             ),
                           ),
@@ -63,6 +89,15 @@ if(image != null) _image = Image.file(image);
                                 hintText: '요리 단계를 설명해주세요.',
                               ),
                               cursorColor: Colors.amber,
+                              onSubmitted: (value) {
+                                setState(() {
+                                  if(recipeStep[_idx].cooking_dc != null){
+                                    _txtController.text=recipeStep[_idx].cooking_dc;
+                                  }
+                                recipeStep[_idx].cooking_dc = value;
+                                _txtController.text = value;
+                                });
+                              },
                             ),
                           )
                         ],
@@ -76,7 +111,7 @@ if(image != null) _image = Image.file(image);
                         iconSize: 40,
                         onPressed: () => {
                           //현재 페이지 내용을 List에 저장
-                          print('현제페이지:$_idx'),
+                          print('현재 페이지:$_idx'),
                           if (_idx > 0)
                             {
                               showCardBloc.dispatch(PageEvent.previous),
@@ -92,17 +127,17 @@ if(image != null) _image = Image.file(image);
                         icon: Icon(Icons.arrow_right),
                         iconSize: 40,
                         onPressed: () => {
-                          if (_idx < _idxMax - 1)
+                          //TOOD:_idxMax 갱신-아니면 다른 방법으로 recipeStep추가
+                          if (_idx >= _idxMax - 1)
                             {
-                              //List<레시피>[_idx]의 다음단계 데이터 출력
-                              showCardBloc.dispatch(PageEvent.next)
-                            }
-                          else
-                            {
-                              showCardBloc.dispatch(PageEvent.next),
                               _idxMax++,
-                              recipe.add(RecipeModel()),
-                            }
+                              recipeStep.add(WriteRecipeProcessModel(
+                                  null, null, null, null, 1234, null, _idx)),
+                              showCardBloc.dispatch(PageEvent.next),
+                              print('레시피 스텝 갯수 : $_idxMax')
+                            },
+                          //List<레시피>[_idx]의 다음단계 데이터 출력
+                          showCardBloc.dispatch(PageEvent.next),
                         },
                       ),
                     )),
