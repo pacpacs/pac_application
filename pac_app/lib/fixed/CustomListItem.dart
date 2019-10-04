562222e28390da3f905050f801bf7d246a8b806b
yanggaeng <carys3115@ajou.ac.kr>
2019-10-04 20:52:33 +0900

    CustomListItem json parse

--- pac_app/lib/fixed/CustomListItem.dart
+++ pac_app/lib/fixed/CustomListItem.dart
@@ -1,9 +1,23 @@
 abstract class ListItem {}
 
 class CustomListItem implements ListItem {
-  final String itemPreview;
-  final String itemTitle;
-  final String itemDescription;
+  String itemPreview;
+  String itemTitle;
+  String itemDescription;
+  int itemID;
+  double percentage;
+  
 
-  CustomListItem(this.itemPreview, this.itemTitle, this.itemDescription);
+  CustomListItem({this.itemPreview, this.itemTitle, this.itemDescription,this.itemID});
+  CustomListItem.withPercentage({this.itemPreview, this.itemTitle, this.itemDescription,this.itemID,this.percentage});
+  
+  factory CustomListItem.fromJsonWithPercentage(Map<String,dynamic> json) {
+      return CustomListItem.withPercentage(
+        itemDescription: json['sumry'],
+        itemTitle:json['recipeNmKo'],
+        itemPreview: json['imgUrl'],
+        itemID:json['recipeId'],
+        percentage:json['percenatge']
+      );
+  }
 }
